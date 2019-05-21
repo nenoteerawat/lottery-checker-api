0e5f074be955c5bf2312279edcfe7f1f94c3b086
nenoteerawat <teerawat@orcsoft.co.th>
2019-05-21 22:00:58 +0700

    init project

---
+++ src/main/java/com/teerawat/lotterychecker/LotteryCheckerApiApplication.java
@@ -0,0 +1,13 @@
+package com.teerawat.lotterychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryCheckerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryCheckerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teerawat/lotterychecker/LotteryCheckerApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.teerawat.lotterychecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LotteryCheckerApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
